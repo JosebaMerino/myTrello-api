d16e01c30c348fb57856dde3af08f6f211dcf187
Joseba Merino <yosiunico@gmail.com>
2020-04-19 19:51:32 +0200

    Removed dist folder

--- dist/controllers/common.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.onlyNotDeleted = { deletionDate: undefined };
-exports.all = {};
-//# sourceMappingURL=common.js.map
